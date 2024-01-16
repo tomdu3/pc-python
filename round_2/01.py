fd78f751cf9da74d70ab408a46e937f1ba1fe491
Tomislav Dukez <tomdu3@ymail.com>
2024-01-16 18:41:44 +0000

    Fix Update solve 01 round 2

--- round_2/01.py
+++ round_2/01.py
@@ -1,10 +1,13 @@
+# CSV Column Averager: Write a function that reads a CSV file and calculates the average of the numbers in a given column.
+# Hint: Use the csv module to read the file and remember to skip the header row. Convert the column values to floats before averaging.
+
 import csv
 
 def table_column_avg(table,headers,column):
 	column_index = headers.index(column)
 	if isinstance(table[0][column_index], str):
 		return f'Error: Column "{column}" has non numeric values'
-	column_average = sum([row[column_index] for row in table])/len(table)
+	column_average = sum([row[column_index]. for row in table])/len(table)
 	return column_average
 
 
@@ -17,7 +20,7 @@ with open('test.csv', 'r') as f:
 	headers = next(csv_content)
 
 	for row in csv_content:
-		row_list = [int(item) if item.isnumeric() else item for item in row]
+		row_list = [float(item) if item.isnumeric() else item for item in row]
 
 		table.append(row_list)
 
