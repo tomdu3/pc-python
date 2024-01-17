06757f9a33020e63bf86e3554ba2657797adebb8
Tomislav Dukez <tomdu3@ymail.com>
2024-01-17 14:20:02 +0000

    Add 2nd solve 04 round 2

--- round_2/04.py
+++ round_2/04.py
@@ -1,13 +1,10 @@
 # Nested Dictionary Updater: Develop a function that takes a nested dictionary and updates a value given its key path as a list.
 
 def nested_dict_updater(dict, key_path, value):
-    last_point = dict
-    for key in key_path:
-        if key != key_path[-1]:
-            last_point = last_point.setdefault(key, {})
-            continue
-        last_point[key_path[-1]] = value
-    return dict
+    if len(key_path) == 1:
+        dict[key_path[0]] = value
+        return dict
+    return {key_path[0]: nested_dict_updater(dict[key_path[0]], key_path[1:], value)}
 
 
 dict_original = {'a': {'b': {'c': 1}}}
