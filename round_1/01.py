8fe8e715f01b56eed4aefc2c3eaf0c5f522f02dd
Tomislav Dukez <tomdu3@ymail.com>
2024-01-25 11:27:59 +0000

    Fix: pep8 issues

--- round_1/01.py
+++ round_1/01.py
@@ -1,12 +1,13 @@
 # Function to Summarize a Text File: Write a function that accepts a filename as a parameter, reads the file, and returns the number of lines, words, and characters in the file.
 
-def  summarize_text_file(filename):
-	with open(filename, 'r') as file:
-		content = file.read()
-	lines = content.split('\n')
-	words = [word for line in lines for word in line.split()]
-	characters = [character for character in content]
+def summarize_text_file(filename):
+    with open(filename, 'r') as file:
+        content = file.read()
+    lines = content.split('\n')
+    words = [word for line in lines for word in line.split()]
+    characters = [character for character in content]
 
-	print(len(lines), len(words), len(characters))
+    print(len(lines), len(words), len(characters))
+
 
 summarize_text_file('kafka-trial.txt')
